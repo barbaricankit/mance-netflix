35714bfd8ac4519ff1bc5464fa145f462ad92820
singhania1993 <ankit.engineer17@gmail.com>
2021-10-11 15:17:54 +0530

    added redirect and title

--- src/features/utils/Loader.jsx
+++ src/features/utils/Loader.jsx
@@ -1,12 +1,14 @@
 import { Oval } from 'react-loading-icons'
 const Loader = ({ status }) => {
   return (
-    status === 'loading' && (
-      <Oval
-        className="absolute top-0 right-0 left-0 bottom-0 m-auto"
-        fill="#e50914"
-      />
-    )
+    <div className="absolute top-0 bottom-0 left-0 right-0 m-auto red-color">
+      {status === 'loading' && (
+        <Oval
+          className="absolute top-0 right-0 left-0 bottom-0 m-auto"
+          stroke="#e50914"
+        />
+      )}
+    </div>
   )
 }
 
